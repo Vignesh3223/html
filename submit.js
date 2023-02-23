598c1dec3d2bd4bbce3d7bb67a33f4e0a97cee35
Vignesh3223 <viketvicky@gmail.com>
2023-02-23 16:57:46 +0530

    2commit

---
+++ submit.js
@@ -0,0 +1,21 @@
+function registration() {
+    var name = document.form1.tname.value;
+    var email = document.form1.mail.value;
+    var password = document.form1.passwd.value;
+    var gender = document.form1.gender.value;
+    var address = document.form1.addr.value;
+    var dob = document.form1.date.value;
+    var profession = document.form1.profession.value;
+    var mobile = document.form1.mob.value;
+    var telephone = document.form1.tele.value;
+
+    document.write(name + "<br>");
+    document.write(email + "<br>");
+    document.write(password + "<br>");
+    document.write(gender + "<br>");
+    document.write(address + "<br>");
+    document.write(dob + "<br>");
+    document.write(profession + "<br>");
+    document.write(mobile + "<br>");
+    document.write(telephone + "<br>");
+}
