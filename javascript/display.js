e2dfb864d55b6eb2fd059e49757d8a00313777fd
Vignesh3223 <viketvicky@gmail.com>
2023-03-04 16:57:57 +0530

    2commit

---
+++ javascript/display.js
@@ -0,0 +1,38 @@
+function upload() {
+    var name = document.myform.name.value;
+    var dob = document.myform.date.value;
+    var gender = document.myform.gen.value;
+    var fathname = document.myform.fname.value;
+    var address = document.myform.addr.value;
+    var mobile = document.myform.mob.value;
+    var landline = document.myform.lan.value;
+    var email = document.myform.mail.value;
+    var college = document.myform.clgname.value;
+    var marks = document.myform.mark.value;
+    var interest = document.myform.interest.value;
+    document.write("<h1><center><u>PERSONAL DETAILS</u></center></h1>");
+    document.write("<br><br>");
+    document.write("<b> Name : </b>" + name);
+    document.write("<br><br>");
+    document.write("<b> DOB : </b>" + dob);
+    document.write("<br><br>");
+    document.write("<b> Gender : </b>" + gender);
+    document.write("<br><br>");
+    document.write("<b> Father Name : </b>" + fathname);
+    document.write("<br><br>");
+    document.write("<b> Address : </b>" + address);
+    document.write("<br><br>");
+    document.write("<b> Mobile Number : </b>" + mobile);
+    document.write("<br><br>");
+    document.write("<b> Landline Number : </b>" + landline);
+    document.write("<br><br>");
+    document.write("<b> E-mail ID : </b>" + email);
+    document.write("<br><br>");
+    document.write("<b> College Name : </b>" + college);
+    document.write("<br><br>");
+    document.write("<b> Marks : </b>" + marks);
+    document.write("<br><br>");
+    document.write("<b> Personal Interest : </b>" + interest);
+    document.write("<br><br>");
+    document.write("<h1><center>THANK YOU</center></h1>");
+}
